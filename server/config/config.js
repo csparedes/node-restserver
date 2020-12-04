d9c227f798258751a534f490232c23abc152e892
Cristian <csparedes1995@gmail.com>
2020-12-04 15:44:34 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,2 +1,21 @@
 //puerto
 process.env.PORT = process.env.PORT || 3000;
+
+
+
+//entorno
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+//base de datos
+
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe'
+// } else {
+urlDB = 'mongodb+srv://user-cafe:3vpJu3oVXNEwk2ru@cluster0.tn7ne.mongodb.net/cafe'
+    // }
+
+process.env.URLDB = urlDB;
